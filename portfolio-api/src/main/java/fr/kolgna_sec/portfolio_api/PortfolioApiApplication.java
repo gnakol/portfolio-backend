3aa6beecc1e718b4b90c3194e57bfa5ad29d738e
gnakol <gna.kolie@yahoo.fr>
2025-02-21 19:30:16 +0100

    Initial commit: Ajout du backend Spring Boot et du fichier docker-compose.yml

---
+++ portfolio-api/src/main/java/fr/kolgna_sec/portfolio_api/PortfolioApiApplication.java
@@ -0,0 +1,13 @@
+package fr.kolgna_sec.portfolio_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioApiApplication.class, args);
+	}
+
+}
