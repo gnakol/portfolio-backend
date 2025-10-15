a7db14e2be125831faba5bdd42a87107a2f0d7ca
gnakol <gna.kolie@yahoo.fr>
2025-10-15 14:16:33 +0200

    scroll top for all bottom navbar

--- portfolio-template/src/app/app-routing.module.ts
+++ portfolio-template/src/app/app-routing.module.ts
@@ -157,7 +157,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration : 'enabled',
+    anchorScrolling : 'enabled',
+    onSameUrlNavigation : 'reload',
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
